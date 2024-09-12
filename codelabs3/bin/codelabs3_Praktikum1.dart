1e3abb4a72f265c789e10d3eee5855abf15b0878
ervanardian17 <ervanardian17@gmail.com>
2024-09-12 10:44:04 +0700

    codelabs3 praktikum 1 if-else

---
+++ codelabs3/bin/codelabs3_Praktikum1.dart
@@ -0,0 +1,19 @@
+import 'package:codelabs3/codelabs3.dart' as codelabs3;
+
+main() {
+  String test = "test2";
+  if (test == "test1") {
+    print("Test1");
+  } else if (test == "test2") {
+    print("Test2");
+  } else {
+    print("Something else");
+  }
+
+  if (test == "test2") print("Test2 again");
+
+  String test1 = "true";
+    if (test1 == 'true') {
+    print("Kebenaran");
+}
+}
