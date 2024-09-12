4ff412e02b1e0000379c0d8b144d259299361917
ervanardian17 <ervanardian17@gmail.com>
2024-09-12 10:32:32 +0700

    first commit

---
+++ codelabs2/bin/codelabs2.dart
@@ -0,0 +1,5 @@
+void main(){
+  for (int i = 18; i > 8; i--){
+    print('nama saya ervan, sekarang umur saya $i');
+  }
+}
---
+++ codelabs3/bin/codelabs3.dart
@@ -0,0 +1,5 @@
+import 'package:codelabs3/codelabs3.dart' as codelabs3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${codelabs3.calculate()}!');
+}
